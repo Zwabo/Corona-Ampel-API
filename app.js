d07b427245ffc81b7e10e9c9f928b3d807c7883e
Sebastian Winkler <sebastian.winkler@students.fh-hagenberg.at>
2020-11-30 16:14:39 +0100

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// eslint-disable-next-line import/no-unresolved
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+
+const app = express();
+
+app.get('/status/:plz', (req, res) => {
+
+  let plz = req.params.plz;
+
+  let gemData = JSON.parse(fs.readFileSync('gemeinden.json'));
+  let gemeinden = gemData.data.find(gemeinde => gemeinde.plz === plz);
+
+  axios.get('https://corona-ampel.gv.at/sites/corona-ampel.gv.at/files/assets/Warnstufen_Corona_Ampel_Gemeinden_aktuell.json')
+      .then(function (response) {
+          let ampelData = response.data;
+          let ampelStatus = ampelData[0].Warnstufen.find(gemeinde => gemeinde.GKZ === gemeinden.gkz);
+          res.send(ampelStatus);
+      })
+      .catch(function (error) {
+          res.send(error);
+      });
+});
+
+// Error handler
+app.use((err, req, res) => {
+  console.error(err);
+  res.status(500).send('Internal Serverless Error');
+});
+
+module.exports = app;
